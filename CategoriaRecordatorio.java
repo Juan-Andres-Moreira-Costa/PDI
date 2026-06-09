3d1648c65dc2e71ea01dfb0171da83a9df3ae5b3
Juan Moreira <juanmoreiracosta2016@gmail.com>
2026-06-08 22:02:26 -0300

    Delete CategoriaRecordatorio.java

--- CategoriaRecordatorio.java
+++
@@ -1,7 +0,0 @@
-package com.utec.sienep.exception;
-
-public class ReglaNegocioException extends RuntimeException {
-    public ReglaNegocioException(String mensaje) {
-        super(mensaje);
-    }
-}
