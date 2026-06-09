df7354508c6c38307bf53a829493fb435f9503c0
Juan Moreira <juanmoreiracosta2016@gmail.com>
2026-06-08 22:02:33 -0300

    Delete CategoriaRecordatorioController.java

--- CategoriaRecordatorioController.java
+++
@@ -1,7 +0,0 @@
-package com.utec.sienep.exception;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
