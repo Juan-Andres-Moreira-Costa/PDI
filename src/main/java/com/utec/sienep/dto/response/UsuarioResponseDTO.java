1e2baecbf092cba5e7b929587a132a4fee4f3aac
Juan Moreira <juanmoreiracosta2016@gmail.com>
2026-07-04 22:42:54 -0300

    Add UsuarioResponseDTO class for user response data

---
+++ src/main/java/com/utec/sienep/dto/response/UsuarioResponseDTO.java
@@ -0,0 +1,109 @@
+package com.utec.sienep.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class UsuarioResponseDTO {
+
+    private Long id;
+    private String username;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private boolean activo;
+    private LocalDateTime fechaAlta;
+    private LocalDateTime ultimoLogin;
+    private int intentosFallidos;
+    private boolean bloqueado;
+    private Set<String> roles;
+
+    public UsuarioResponseDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDateTime getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(LocalDateTime fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public LocalDateTime getUltimoLogin() {
+        return ultimoLogin;
+    }
+
+    public void setUltimoLogin(LocalDateTime ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+
+    public int getIntentosFallidos() {
+        return intentosFallidos;
+    }
+
+    public void setIntentosFallidos(int intentosFallidos) {
+        this.intentosFallidos = intentosFallidos;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    public void setBloqueado(boolean bloqueado) {
+        this.bloqueado = bloqueado;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
